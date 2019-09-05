8e4627740425fb9c37c2897ed37790ae9f45b743
Ryan <rfennell001@gmail.com>
2019-09-05 16:14:16 -0400

    Completed js and updated styles

---
+++ js/countdown.js
@@ -0,0 +1,33 @@
+window.onload = function countdown(){
+    //create empty date variables for today and future dates
+    const currentDate = new Date();
+    const futureDate = new Date();
+    
+    //return the sum of milliseconds since Jan 1 1970 to be used to calculate time until webinar.
+    const currentTime = currentDate.getTime(); 
+
+    //set future date 5 days into the future
+    //NOTE: I wasn't completely sure if today would count as one of the 5 days until the webinar per the instructions. If so it would be set to getDate()+4)
+    futureDate.setDate(futureDate.getDate()+5);
+
+    //set future date to midnight and return the sum of milliseconds since Jan 1 1970 to that date
+    const futureTime  = futureDate.setHours(24,0,0,0) 
+    
+    //subtract the current date from the future date to get the number of milliseconds left until the webinar.
+    const difference = futureTime - currentTime;
+
+    //use some math to calculate the days,hours,min,sec left in the difference between current and future date.
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    //output the data into their respective containers.
+    document.querySelector('#dateData-days').innerHTML = days 
+    document.querySelector('#dateData-hours').innerHTML = hours 
+    document.querySelector('#dateData-minutes').innerHTML = minutes 
+    document.querySelector('#dateData-seconds').innerHTML = seconds 
+    
+    //set the function to rerun every second
+    setTimeout( countdown, 1000); 
+}
---
+++ js/form.js
@@ -0,0 +1,76 @@
+//listen for the first checkbox to be checked and set the second to checked or unchecked accordingly.
+document.querySelector('#checkboxHeader').addEventListener('click',function(){
+    if (document.querySelector('#checkboxHeader').checked === true){
+        document.querySelector('#checkboxFooter').checked = true;
+    }
+    else{
+        document.querySelector('#checkboxFooter').checked = false;
+    }
+})
+
+//listen for the second checkbox to be checked and set the first to checked or unchecked accordingly.
+document.querySelector('#checkboxFooter').addEventListener('click',function(){
+    if (document.querySelector('#checkboxFooter').checked === true){
+        document.querySelector('#checkboxHeader').checked = true;
+    }
+    else{
+        document.querySelector('#checkboxHeader').checked = false;
+    }
+})
+
+//set an array of each submit button. If there were more buttons we didn't want to click I'd use something like classname instead.
+const buttonArray = document.querySelectorAll('button[type="submit"]');
+
+//associate an event listener with each of the buttons in the array.
+buttonArray.forEach(function(button){
+    
+    button.addEventListener('click', function(event){
+
+        //prevent the page from reloading upon submit
+        event.preventDefault()
+
+        //set an array containing each checkbox and their current attributes.
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+
+        //check that both checkboxes are checked before continuing.
+        if (checkboxes[0].checked === true && checkboxes[1].checked === true){   
+            
+            try{
+                //create request object to be sent
+                const request = new XMLHttpRequest()
+
+                //use a GET request as we are not sending any data, and set the async boolean to 'true'
+                request.open('GET', 'https://bl45immth4.execute-api.us-east-1.amazonaws.com/production/', true)
+
+                //tell the http request to not send any data.
+                request.send()
+                
+                request.onload = function(){
+
+                    //parse the response into a JSON object and then single out the body.
+                    const res = JSON.parse(this.response);
+                    const resBody = res.body
+
+                    //parse the body's contents into another JSON object
+                    const bodyJson= JSON.parse(resBody);
+                    
+                    //select each of the 'submit-response' tags and set their value to that of submitok
+                    const submitResponse = document.querySelectorAll('.submit-response')
+                    submitResponse.forEach(function(e){
+                        e.style.display='block' //submit-response is hidden by default to avoid affecting the layout
+                        e.innerHTML=bodyJson.submitok
+                    })
+                }
+            }
+            //helpful error log in case of any issues
+            catch(error){
+                console.log(error)
+            }
+        }
+
+        //alert the user to check the checkbox in order to continue.
+        else{
+            alert('Please select that you would like a free newspaper to reserve your live webinar.')
+        }
+    })
+})
